14c7a6498a405f1108e5ef1de9c9ff306527dbe0
Oleh Semennykov <sumcue@gmail.com>
2021-08-15 21:59:22 +0300

    add tmplates

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 // import './sass/main.scss';
-import menuTpl from './templates/menu.hbs';
+// import menuFirst from './templates/menu.hbs';
+import menuTpl from './templates/menuTpl.hbs';
 import  './styles.css';
 import menu  from './menu.json';
-console.log(menuTpl);
-
-
+// console.log(menuFirst);
+// console.log(menuTpl);
+// console.log(menuTpl(menu));
 const menuContainer  = document.querySelector(".js-menu");
-console.log(menuContainer);
+// // console.log(menuContainer);
 const makeMenuCard = createMenuCardMarkup(menu);
 
 menuContainer.insertAdjacentHTML('beforeend', makeMenuCard );
 
 function createMenuCardMarkup (menu) {
     // return  menu.map(menu => menuTpl(menu)) 
-    return  menu.map(menuTpl) 
-    .join('');
+    // return  menu.map(menuFirst) 
+    // .join('');
+    return menuTpl(menu);
 }
